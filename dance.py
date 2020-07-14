8dc1c91863457ef0b15869e4ff78e8d41475ade8
Bee <pizzaman8099@gmail.com>
2020-07-14 11:03:52 -0700

    Change control flow in map_dir_to_pair

--- dance.py
+++ dance.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from typing import List, Tuple
 
 """A robot has been given a list of movement instructions. Each instruction
 is either left, right, up or down, followed by a distance to move. The
@@ -7,11 +9,30 @@ the instructions ["right 10", "up 50", "left 30", "down 10"], it will end
 up 20 left and 40 up from where it started, so you should return [-20, 40].
 """
 
-from functools import reduce
-from typing import List, Tuple
+def map_dir_to_pair(d_val_str) -> Tuple[int, int]:
+    direction, v_str = d_val_str.split(' ')
+    val = int(v_str)
 
-"""Old
-def add_val(acc, d_val):
+    if direction == "up":
+        res = (0, val)
+    elif direction == "down":
+        res = (0, -val)
+    elif direction == "right":
+        res = (val, 0)
+    elif direction == "left":
+        res = (-val, 0)
+    return res
+
+def add_val(acc, d_val) -> Tuple[int, int]:
+    acc_left, acc_right = acc
+    cur_left, cur_right = map_dir_to_pair(d_val)
+    return (acc_left + cur_left, acc_right + cur_right)
+
+def mr_roboto(instructions) -> List[int]:
+    return list(reduce(add_val, instructions, (0, 0)))
+
+## Old version
+def add_val_old(acc, d_val):
     direction, v = d_val.split(' ')
     val = int(v)
     switch = {
@@ -23,26 +44,6 @@ def add_val(acc, d_val):
     add_val = switch[direction]
     return [acc[0] + add_val[0], acc[1] + add_val[1]]
 
-def mr_roboto(instructions):
+def mr_roboto_old(instructions):
     return reduce(add_val, instructions, [0, 0])
-"""
-
-def map_dir_to_pair(d_val_str) -> Tuple[int, int]:
-    direction, v_str = d_val_str.split(' ')
-    val = int(v_str)
-    switch = {
-        "up": (0, val),
-        "down": (0, -val),
-        "right": (val, 0),
-        "left": (-val, 0)
-    }
-    return switch[direction]
-
-def add_val(acc, d_val) -> Tuple[int, int]:
-    old_left, old_right = acc
-    cur_left, cur_right = map_dir_to_pair(d_val)
-    return (old_left + cur_left, old_right + cur_right)
-
-def mr_roboto(instructions) -> List[int]:
-    return list(reduce(add_val, instructions, (0, 0)))
 
